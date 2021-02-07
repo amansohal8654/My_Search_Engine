718d2f6484916f09ee80c019b409329d40cf3d25
amansohal8654 <amansohal8654@gmail.com>
2021-02-08 12:52:32 +1300

    listing data in json format

--- Crawler.py
+++ Crawler.py
@@ -1,10 +1,34 @@
+import json
 import requests
 from bs4 import BeautifulSoup
-
+# bas url 
 first_url = "https://stackoverflow.com/questions"
 
-response = requests.get(first_url)
-content = BeautifulSoup(response.text, 'html.parser')
-links = content.findAll('a', {'class' : 'question-hyperlink'})
+# data fatching function
+def featch(url):
+    try:
+        # http request for given url
+        response = requests.get(url)
+        # errorn handling
+    except response.exception.ConnectionError:
+        print('Give URL : "%s" is not available!' % url)
+    #parse content
+    content = BeautifulSoup(response.text, 'html.parser')
+    # extract question content
+    description = content.findAll('div', {'class' : 'excerpt'})
+    # extract question description
+    links = content.findAll('a', {'class' : 'question-hyperlink'})
 
-print(links)
+    #print(len(links), len(description))
+    # loop over stack overflows question list
+    for index in range(0, len(description)):
+        # storing data
+        question = {
+            'title': links[index].text,
+            'url' : links[index]['href'],
+            'description' : description[index].text.strip().replace('\n', '')
+        }
+        #print data in json format
+        print(json.dumps(question, indent = 2))
+
+featch(first_url)
