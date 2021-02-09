7a40fcc541f66fae6d9f05159df0e42b420a593f
amansohal8654 <amansohal8654@gmail.com>
2021-02-10 11:20:51 +1300

    cleaning data

--- Crawler.py
+++ Crawler.py
@@ -1,12 +1,15 @@
 import json
 import requests
+import re
+import string
 from bs4 import BeautifulSoup
 # bas url 
-first_url = "https://hackr.io/"
-links = []
-course = []
+first_url = "https://bola.kompas.com/"
+
 # data fatching function
 def featch(url):
+    links = []
+    documents = []
     try:
         # http request for given url
         response = requests.get(url)
@@ -15,15 +18,42 @@ def featch(url):
         print('Give URL : "%s" is not available!' % url)
     #parse content
     content = BeautifulSoup(response.text, 'html.parser')
-    # extract question description
-    link = content.find_all('a', {'class' : 'topic-grid-item'})
-    for i in link:
+    for i in content.find('div', {'class':'most__wrap'}).find_all('a'):
+        i['href'] = i['href'] + '?page=all'
         links.append(i['href'])
 
-    r = requests.get(links[0])
-    soup = BeautifulSoup(r.content, 'html.parser')
-    data = soup.find("script").find_all("mainEntity")
-    print(data)
+    for i in links:
+        # Make a request to the link
+        r = requests.get(i)
+    
+        # Initialize BeautifulSoup object to parse the content 
+        soup = BeautifulSoup(r.content, 'html.parser')
+    
+        # Retrieve all paragraphs and combine it as one
+        sen = []
+        for i in soup.find('div', {'class':'read__content'}).find_all('p'):
+            sen.append(i.text)
+            # Add the combined paragraphs to documents
+        documents.append(' '.join(sen))
+    
+    docClean = []
+    for d in documents:
+        # Remove Unicode
+        document_test = re.sub(r'[^\x00-\x7F]+', ' ', d)
+        # Remove Mentions
+        document_test = re.sub(r'@\w+', '', document_test)
+        # Lowercase the document
+        document_test = document_test.lower()
+        # Remove punctuations
+        document_test = re.sub(r'[%s]' % re.escape(string.punctuation), ' ', document_test)
+        # Lowercase the numbers
+        document_test = re.sub(r'[0-9]', '', document_test)
+        # Remove the doubled space
+        document_test = re.sub(r'\s{2,}', ' ', document_test)
+        docClean.append(document_test)
+    return docClean
 
-featch(first_url)
-#print(links)
+
+   
+
+print(featch(first_url))
